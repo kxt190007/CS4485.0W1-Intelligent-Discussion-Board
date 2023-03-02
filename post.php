5255e9cca9d37f5076d9bdffccf08dfad6292546
kevinlim711 <125513274+kevinlim711@users.noreply.github.com>
2023-03-01 20:15:52 -0600

    post connect to db

--- post.php
+++ post.php
@@ -2,11 +2,17 @@
 include 'connect.php';
 
 $connect = Connect();
-$sql = "INSERT INTO Posts (PostID, UserID, PostStatus, PostBody, PostTitle)
-VALUES(1, 1, 1, 'test', 'test')";
+$userID = '1'; //placeholder until user ID can be found
+$postStatus = '1'; // 1 will be an unresolved post
+$posttitle = $_POST['posttitle'];
+$postcontent = $_POST['postcontent'];
+$posttag = $_POST['tag'];
+$sql = "INSERT INTO Posts (UserID, PostStatus, PostBody, PostTitle, PostTag)
+VALUES ('$userID','$postStatus','$postcontent','$posttitle','$posttag')";
+
 
 if ($connect->query($sql) === TRUE) {
-    echo "Record updated successfully";
+    echo "Post Created Successfully. Redirecting to home page";
 } else {
     echo "Error updating record: " . $connect->error;
 }
@@ -14,3 +20,8 @@ if ($connect->query($sql) === TRUE) {
 CloseConnect($connect);
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="refresh" content="2; URL=index.html" />
+</head>
